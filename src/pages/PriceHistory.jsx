4941b9a44f751637e894257e7bdf367566474799
cyclotrons-hamhung <connor.mchugh5@gmail.com>
2020-05-07 20:45:09 +1000

    Row clicks working

--- src/pages/PriceHistory.jsx
+++ src/pages/PriceHistory.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 import { AgGridReact } from "ag-grid-react";
@@ -6,6 +6,8 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-material.css";
 
 import { Container, Row } from "reactstrap";
+
+import { useHistory } from "react-router-dom";
 
 const base_url = "http://131.181.190.87:3000/stocks/";
 
@@ -24,127 +26,127 @@ const base_url = "http://131.181.190.87:3000/stocks/";
 
 // rowNode.setDataValue("timestamp", year + "/" + month + "/" + day);
 
-class PriceHistory extends Component {
+export default function PriceHistory(props) {
   // Setup the table data
-  constructor(props) {
-    super(props);
+  const [rowData, setRowData] = useState([]);
+  const [gridApi, setGridApi] = useState(null);
 
-    this.state = {
-      columnDefs: [
-        {
-          headerName: "Name",
-          field: "name",
-        },
-        {
-          headerName: "Symbol",
-          field: "symbol",
-        },
-        {
-          headerName: "Industry",
-          field: "industry",
-        },
-        {
-          headerName: "Open",
-          field: "open",
-        },
-        {
-          headerName: "Close",
-          field: "close",
-        },
-        {
-          headerName: "High",
-          field: "high",
-        },
-        {
-          headerName: "Low",
-          field: "low",
-        },
-        {
-          headerName: "Volumes",
-          field: "volumes",
-        },
-      ],
-      rowData: [
-        {
-          id: "row",
-          symbol: "",
-          name: "",
-          industry: "",
-          open: "",
-          high: "",
-          low: "",
-          close: "",
-          volumes: "",
-        },
-      ],
-      getRowNodeId: function (data) {
-        return data.id;
-      },
-    };
+  let history = useHistory();
+
+  const columnDefs = [
+    {
+      headerName: "Date",
+      field: "date",
+    },
+    {
+      headerName: "Open",
+      field: "open",
+    },
+    {
+      headerName: "Close",
+      field: "close",
+    },
+    {
+      headerName: "Low",
+      field: "low",
+    },
+    {
+      headerName: "High",
+      field: "high",
+    },
+    {
+      headerName: "Volumes",
+      field: "volumes",
+    },
+  ];
+
+  // let rowData = [
+  //   {
+  //     id: "row",
+  //     open: "",
+  //     high: "",
+  //     low: "",
+  //     close: "",
+  //     volumes: "",
+  //   },
+  // ];
+
+  function getRowNodeId(data) {
+    return data.id;
   }
 
-  onGridReady = (params) => {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    params.api.sizeColumnsToFit();
   };
 
-  componentDidMount = () => {
-    let url = base_url + this.props.stock_symbol;
-    let rowNode = this.gridApi.getRowNode("row");
-    fetch(url)
-      .then((result) => result.json())
-      .then((out) => {
-        rowNode.setDataValue("symbol", out.symbol);
-        rowNode.setDataValue("name", out.name);
-        rowNode.setDataValue("industry", out.industry);
-        rowNode.setDataValue("open", out.open);
-        rowNode.setDataValue("high", out.high);
-        rowNode.setDataValue("low", out.low);
-        rowNode.setDataValue("close", out.close);
-        rowNode.setDataValue("volumes", out.volumes);
-      });
-  };
 
-  // componentDidMount = () => {
-  //   let url = base_url + this.props.stock_symbol;
-  //   console.log(url);
-
+  // useEffect(() => {
+  //   let rowNode = gridApi.getRowNode("row");
+  //   let url = base_url + props.stock_symbol;
   //   fetch(url)
   //     .then((result) => result.json())
-  //     .then((rowData) => this.setState({ rowData }));
-  // };
+  //     .then((stock) => {
+  //       rowNode.setDataValue("date", stock.timestamp);
+  //       rowNode.setDataValue("open", stock.open);
+  //       rowNode.setDataValue("high", stock.high);
+  //       rowNode.setDataValue("low", stock.low);
+  //       rowNode.setDataValue("close", stock.close);
+  //       rowNode.setDataValue("volumes", stock.volumes);
+  //     });
+  // });
+
+
+  // useEffect(() => {
+  //   let url = base_url + props.stock_symbol;
+  //   fetch(url)
+  //     .then((result) => result.json)
+  //     .then(data =>
+  //       data.map((stock) => {
+  //         return {
+  //             date: stock.timestamp,
+  //             open: stock.open,
+  //             close: stock.close,
+  //             low: stock.low,
+  //             high: stock.high,
+  //             volumes: stock.volumes,
+  //           };
+  //       })
+  //     )
+  //     .then((stocks) => setRowData(stocks));
+  // });
+
+
+  useEffect(() => {
+    let url = base_url + props.stock_symbol;
+    let stock_file = fetch(url);
+    console.log(JSON.stringify(stock_file));
+  })
 
   // Render the page
-  render() {
-    return (
-      <div className="page_content">
-        <Container>
-          <p>{this.props.stock_symbol}</p>
+  return (
+    <div className="page_content">
+      <Container>
+        <p>{props.stock_symbol}</p>
 
-          <Row>
-            <div
-              className="ag-theme-material"
-              style={{
-                width: "100%",
-              }}
-            >
-              <AgGridReact
-                domLayout={"autoHeight"}
-                pagination={true}
-                paginationPageSize={15}
-                columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData}
-                animateRows={true}
-                onGridReady={this.onGridReady}
-                getRowNodeId={this.state.getRowNodeId}
-                rowSelection={this.state.rowSelection}
-              ></AgGridReact>
-            </div>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+        <Row>
+          <div
+            className="ag-theme-material"
+            style={{
+              width: "100%",
+            }}
+          >
+            <AgGridReact
+              domLayout={"autoHeight"}
+              columnDefs={columnDefs}
+              rowData={rowData}
+              animateRows={true}
+              onGridReady={onGridReady}
+              getRowNodeId={getRowNodeId}
+            ></AgGridReact>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
 }
-
-export default PriceHistory;
--- src/pages/Stocks.jsx
+++ src/pages/Stocks.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 import { AgGridReact } from "ag-grid-react";
@@ -16,144 +16,141 @@ import {
   InputGroup,
   InputGroupAddon,
 } from "reactstrap";
-import { useHistory, Redirect } from "react-router-dom";
+
+import { useHistory } from "react-router-dom";
 
 const base_url = "http://131.181.190.87:3000/stocks/";
 const all_url = "http://131.181.190.87:3000/stocks/symbols?";
 const industry_url = "http://131.181.190.87:3000/stocks/symbols?industry=";
 
-class Stocks extends Component {
-  // Setup the table data
-  constructor(props) {
-    super(props);
+export default function Stocks() {
+  const [rowData, setRowData] = useState([]);
+  const [gridApi, setGridApi] = useState(null);
 
-    this.state = {
-      columnDefs: [
-        {
-          headerName: "Name",
-          field: "name",
-        },
-        {
-          headerName: "Symbol",
-          field: "symbol",
-        },
-        {
-          headerName: "Industry",
-          field: "industry",
-        },
-      ],
-      rowSelection: "single",
-      rowData: [],
-    };
-  }
+  let history = useHistory();
 
-  onGridReady = (params) => {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
+  const columnDefs = [
+    {
+      headerName: "Name",
+      field: "name",
+    },
+    {
+      headerName: "Symbol",
+      field: "symbol",
+    },
+    {
+      headerName: "Industry",
+      field: "industry",
+    },
+  ];
+
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    params.api.sizeColumnsToFit();
   };
 
-  onFirstDataRendered = () => {
-    this.gridApi.sizeColumnsToFit();
-  };
-
-  // Handle 
-  onSelectionChanged = () => {
-    let selectedRow = this.gridApi.getSelectedRows();
-    console.log(selectedRow[0].symbol);
-    let url = base_url + selectedRow[0].symbol;
-    console.log(url);
-    return (
-      <Redirect to="/AAL" />
-    );
-  };
+  // USE ME TO DEAL WITH ERRORS:
+  // https://stackoverflow.com/questions/51859358/how-to-read-json-file-with-fetch-in-javascript
 
   // Fetch data from URL provided as parameter an set it to the ag-grid
-  setValues = (url) => {
+  function setValues(url) {
     fetch(url)
       .then((result) => result.json())
-      .then((rowData) => this.setState({ rowData }));
-  };
-
-  // Initially, can the setValues function with the url to display all data
-  componentDidMount() {
-    this.setValues(all_url);
+      .then((data) =>
+        data.map((stock) => {
+          return {
+            symbol: stock.symbol,
+            name: stock.name,
+            industry: stock.industry,
+          };
+        })
+      )
+      .then((stocks) => setRowData(stocks));
   }
+
+  // Handle the cell rows being clicked
+  function onRowClicked(row) {
+    // console.log(row.data.symbol);
+    history.push("/stocks/" + row.data.symbol);
+  }
+
+  // Initially, call the setValues function with the base url to display all data
+  useEffect(() => {
+    setValues(all_url);
+  }, []);
 
   // Render the page
-  render() {
-    return (
-      <div className="page_content">
-        <Container>
-          <Row>
-            <Col></Col>
-            <Col>
-              <Label for="industry">Filter by industry:</Label>
-            </Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col xs="auto">
-              <InputGroup>
-                <Form
-                  inline
-                  onSubmit={(event) => {
-                    event.preventDefault();
-                    this.setValues(all_url);
-                  }}
-                >
-                  <InputGroupAddon addonType="prepend">
-                    <Button color="secondary">Clear Filter</Button>
-                  </InputGroupAddon>
-                </Form>
-                <Form
-                  inline
-                  onSubmit={(event) => {
-                    event.preventDefault();
-                    let url_suffix = event.target.elements.industry.value.replace(
-                      / /g,
-                      "%20"
-                    );
+  return (
+    <div className="page_content">
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Label for="industry">Filter by industry:</Label>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col xs="auto">
+            <InputGroup>
+              <Form
+                inline
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  setValues(all_url);
+                }}
+              >
+                <InputGroupAddon addonType="prepend">
+                  <Button color="secondary">Clear Filter</Button>
+                </InputGroupAddon>
+              </Form>
+              <Form
+                inline
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  let url_suffix = event.target.elements.industry.value.replace(
+                    / /g,
+                    "%20"
+                  );
 
-                    let url = industry_url + url_suffix;
-                    this.setValues(url);
-                  }}
-                >
-                  <Input type="text" name="industry" id="industry" />
-                  <InputGroupAddon addonType="append">
-                    <Button color="secondary">Apply Filter</Button>
-                  </InputGroupAddon>
-                </Form>
-              </InputGroup>
-            </Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <div
-              className="ag-theme-material"
-              style={{
-                width: "100%",
-              }}
-            >
-              <AgGridReact
-                domLayout={"autoHeight"}
-                pagination={true}
-                paginationPageSize={15}
-                columnDefs={this.state.columnDefs}
-                rowData={this.state.rowData}
-                animateRows={true}
-                getRowNodeId={this.state.getRowNodeId}
-                onGridReady={this.onGridReady}
-                onFirstDataRendered={this.onFirstDataRendered.bind(this)}
-                onSelectionChanged={this.onSelectionChanged.bind(this)}
-                rowSelection={this.state.rowSelection}
-              ></AgGridReact>
-            </div>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+                  let url = industry_url + url_suffix;
+                  setValues(url);
+                }}
+              >
+                <Input type="text" name="industry" id="industry" />
+                <InputGroupAddon addonType="append">
+                  <Button color="secondary">Apply Filter</Button>
+                </InputGroupAddon>
+              </Form>
+            </InputGroup>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <div
+            className="ag-theme-material"
+            style={{
+              width: "100%",
+            }}
+          >
+            <AgGridReact
+              domLayout={"autoHeight"}
+              pagination={true}
+              paginationPageSize={15}
+              columnDefs={columnDefs}
+              rowData={rowData}
+              animateRows={true}
+              // getRowNodeId={getRowNodeId}
+              onGridReady={onGridReady}
+              onRowClicked={onRowClicked}
+              // onFirstDataRendered={onFirstDataRendered.bind(this)}
+              // onSelectionChanged={onSelectionChanged.bind(this)}
+              // rowSelection={rowSelection}
+            ></AgGridReact>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
 }
-
-export default Stocks;
